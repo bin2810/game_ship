ad1ded510cb81e4100be0400157c507774eaae93
vu <bin200528@gmail.com>
2025-07-12 19:51:15 +0700

    up

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById('start').addEventListener("click", () => {
+    
+    const introducts = document.getElementById('introducts');
+    const game_frame = document.getElementById('game_frame');
+
+    introducts.classList.add("hidden");
+    game_frame.classList.remove("hidden");
+
+});
+
+
